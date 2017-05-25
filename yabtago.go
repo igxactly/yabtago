8e7d6ea2e5516affa64d4784d55c963e33f7db55
Ingu Kang <igxactly@gmail.com>
2017-05-26 01:58:41 +0900

    Add argument parsing, code rearrange/cleanup

--- yabtago.go
+++ yabtago.go
@@ -7,24 +7,14 @@ import (
 	"fmt"
 	"io"
 	"os"
+
+	"github.com/alexflint/go-arg"
 )
 
-// MinMaxMean is ...
-type MinMaxMean struct {
-	Min  map[string]uint64  `json:"min"`
-	Max  map[string]uint64  `json:"max"`
-	Mean map[string]float64 `json:"mean"`
-}
-
-// BlktraceResult is
-type BlktraceResult struct {
-	R MinMaxMean
-	W MinMaxMean
-}
-
-// BlktraceRecord is
+// BlktraceRecord represents a blktrace record.
 type BlktraceRecord struct {
 	// Magic                        uint32
+
 	Seq                          uint32
 	Time, Sector                 uint64
 	Bytes, Action, Pid, Dev, CPU uint32
@@ -33,15 +23,29 @@ type BlktraceRecord struct {
 	PduData string
 }
 
-// ToString -
-func (r *BlktraceRecord) ToString() string {
+// String -
+func (r *BlktraceRecord) String() string {
 	return fmt.Sprintf("BlktraceRecord:%d %d %d %d 0x%08x %d %d %d %d %d %s",
 		r.Seq, r.Time, r.CPU, r.Pid, r.Action,
 		r.Dev, r.Sector, r.Bytes, r.Err, r.PduLen, r.PduData)
+
 	/* TODO: replace non-readable chars from r.Pdu_data /[\x00-\x08\x0A-\x1F\x7F]/ */
 }
 
-// BlktraceStatistics is ...
+// MinMaxMean -
+type MinMaxMean struct {
+	Min  map[string]uint64  `json:"min"`
+	Max  map[string]uint64  `json:"max"`
+	Mean map[string]float64 `json:"mean"`
+}
+
+// BlktraceResult -
+type BlktraceResult struct {
+	R MinMaxMean
+	W MinMaxMean
+}
+
+// BlktraceStatistics -
 type BlktraceStatistics struct {
 	traceBatches map[uint64]map[string]*BlktraceRecord
 	numBatches   uint64
@@ -51,7 +55,7 @@ type BlktraceStatistics struct {
 	maximums map[string]uint64
 }
 
-// NewBlktraceStatistics is ...
+// NewBlktraceStatistics -
 func NewBlktraceStatistics() *BlktraceStatistics {
 	newObj := BlktraceStatistics{numBatches: 0}
 
@@ -137,12 +141,12 @@ func (s *BlktraceStatistics) AddRecord(r *BlktraceRecord) {
 
 		if drvToQ < 0 {
 			fmt.Printf("Warning: minus!! %d", drvToQ)
-			fmt.Printf(r.ToString())
+			fmt.Printf(r.String())
 		}
 
 		if cToDrv < 0 {
 			fmt.Printf("Warning: minus!! %d", cToDrv)
-			fmt.Printf(r.ToString())
+			fmt.Printf(r.String())
 		}
 
 		s.totals["DRV-Q"] += drvToQ
@@ -190,33 +194,30 @@ func (s *BlktraceStatistics) GetAvg() map[string]float64 {
 	return avgs
 }
 
-// ToString is
-func (s *BlktraceStatistics) ToString() string {
-	cnt := s.numBatches
+// String is
+func (s *BlktraceStatistics) String() string {
 	var avgDrvToQ, avgCToDrv float64
 
-	if cnt > 0 {
-		avgDrvToQ = float64(s.totals["DRV-Q"]) / float64(cnt)
-		avgCToDrv = float64(s.totals["C-DRV"]) / float64(cnt)
-	} else {
-		avgDrvToQ, avgCToDrv = 0, 0
-	}
-
 	if s.numBatches > 0 {
+		avgDrvToQ = float64(s.totals["DRV-Q"]) / float64(s.numBatches)
+		avgCToDrv = float64(s.totals["C-DRV"]) / float64(s.numBatches)
+
 		// TODO: rewrite this ugly statement
 		return fmt.Sprintf("BlktraceStatistics: cnt=%d\n  avg DRV-Q=%fus C-DRV=%fus\n  min DRV-Q=%fus C-DRV=%fus\n  max DRV-Q=%fus C-DRV=%fus",
-			cnt, float64(avgDrvToQ)/1000.0, float64(avgCToDrv)/1000.0, float64(s.minimums["DRV-Q"])/1000.0, float64(s.minimums["C-DRV"])/1000.0, float64(s.maximums["DRV-Q"])/1000.0, float64(s.maximums["C-DRV"])/1000.0)
+			s.numBatches, float64(avgDrvToQ)/1000.0, float64(avgCToDrv)/1000.0, float64(s.minimums["DRV-Q"])/1000.0, float64(s.minimums["C-DRV"])/1000.0, float64(s.maximums["DRV-Q"])/1000.0, float64(s.maximums["C-DRV"])/1000.0)
 	}
-	return fmt.Sprintf("BlktraceStatistics: cnt=%d\n  Nothing is collected", cnt)
+
+	avgDrvToQ, avgCToDrv = 0, 0
+	return fmt.Sprintf("BlktraceStatistics: cnt=%d\n  Nothing is collected", s.numBatches)
 }
 
-// ReadAndParseOneRecord is
-func ReadAndParseOneRecord(reader io.Reader) (*BlktraceRecord, error) {
-	// # # # # # #
-	// # Reference:
-	// #   struct blk
-	// #    - definition is in include/uapi/linux/blktrace_api.h
-	// #
+// ReadBlktraceRecord reads one blktrace record from reader and create/return a BlktraceRecord.
+func ReadBlktraceRecord(reader io.Reader) (*BlktraceRecord, error) {
+	// Reference: blktrace record structure
+	//     - definition from blktrace_api.h
+	//
+	// blktrace log file is written in this structure.
+	//
 	// # __u32 magic;        /* MAGIC << 8 | version */
 	// # __u32 sequence;     /* event number */
 	// # __u64 time;     /* in microseconds */ ??? it seems it is not US but NS
@@ -228,6 +229,8 @@ func ReadAndParseOneRecord(reader io.Reader) (*BlktraceRecord, error) {
 	// # __u32 cpu;      /* on what cpu did it happen */
 	// # __u16 error;        /* completion error */
 	// # __u16 pdu_len;      /* length of data after this trace */
+	//
+	// and PDU data follows.
 
 	r := new(BlktraceRecord)
 
@@ -243,45 +246,37 @@ func ReadAndParseOneRecord(reader io.Reader) (*BlktraceRecord, error) {
 		return buf[0:n]
 	}
 
-	l := 8*2 + 4*7 + 2*2
+	// Read a blktrace record, excluding pdu_data
+	l := 8*2 + 4*7 + 2*2 // 64bit*2, 32bit*7, 16bit*2
 	readN(l)
 
+	// assign each fields
 	st := 0
-
-	_ = u32le(buf[st : st+4]) // d_magic
+	_ = u32le(buf[st : st+4]) // omit d_magic
 	st += 4
-
 	r.Seq = u32le(buf[st : st+4])
 	st += 4
-
 	r.Time = u64le(buf[st : st+8])
 	st += 8
-
 	r.Sector = u64le(buf[st : st+8])
 	st += 8
-
 	r.Bytes = u32le(buf[st : st+4])
 	st += 4
-
 	r.Action = u32le(buf[st : st+4])
 	st += 4
-
 	r.Pid = u32le(buf[st : st+4])
 	st += 4
-
 	r.Dev = u32le(buf[st : st+4])
 	st += 4
-
 	r.CPU = u32le(buf[st : st+4])
 	st += 4
-
 	r.Err = u16le(buf[st : st+2])
 	st += 2
-
 	r.PduLen = u16le(buf[st : st+2])
 	st += 2
 
 	if r.PduLen > 0 {
+		// TODO: handle non-char bytes
 		r.PduData = string(readN(int(r.PduLen)))
 	} else {
 		r.PduData = ""
@@ -290,30 +285,39 @@ func ReadAndParseOneRecord(reader io.Reader) (*BlktraceRecord, error) {
 	return r, err
 }
 
-//#################
-//# main program flow starts here
-func main() {
+func parse(input *bufio.Reader, output *bufio.Writer) {
 	var err error
+	var r *BlktraceRecord
 
-	// Signal.trap("PIPE", "EXIT")
+	for true {
+		err = nil
+		r, err = ReadBlktraceRecord(input)
+
+		if !(err == nil) {
+			// TODO: check of any error occured while reading record.
+			// if err is not eof error {
+			// 	fmt.Println("parse(): something went wrong while reading blktrace record")
+			// }
+			break
+		}
+
+		fmt.Println(r.String())
+	}
+}
+
+func report(input *bufio.Reader, output *bufio.Writer) {
+	var err error
+	var r *BlktraceRecord
 
 	readStats := NewBlktraceStatistics()
 	writeStats := NewBlktraceStatistics()
 
-	fileInput, err := os.Open(os.Args[1])
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	defer fileInput.Close()
-
-	reader := bufio.NewReader(fileInput)
-
 	for true {
 		err = nil
-		r, err := ReadAndParseOneRecord(reader)
+		r, err = ReadBlktraceRecord(input)
 
 		if !(err == nil) {
+			// TODO: check of any error occured while reading record.
 			break
 		}
 
@@ -333,22 +337,22 @@ func main() {
 		} else if rw == "W" {
 			writeStats.AddRecord(r)
 		} else {
-			fmt.Println(r.ToString())
+			fmt.Println(r.String())
 		}
 	}
 
 	fmt.Print("\n\n\n")
-	fmt.Println("yabtar_read_stat:", readStats.ToString())
-	fmt.Println("yabtar_write_stat:", writeStats.ToString())
+	fmt.Println("yabtar_read_stat:", readStats.String())
+	fmt.Println("yabtar_write_stat:", writeStats.String())
 	fmt.Print("\n\n\n")
 
-	fileOutput, err := os.OpenFile(os.Args[2], os.O_RDWR|os.O_CREATE, 0644)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	defer fileOutput.Close()
-	writer := bufio.NewWriter(fileOutput)
+	// fileOutput, err := os.OpenFile(os.Args[2], os.O_RDWR|os.O_CREATE, 0644)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	os.Exit(1)
+	// }
+	// defer fileOutput.Close()
+	// writer := bufio.NewWriter(fileOutput)
 
 	jData := BlktraceResult{}
 
@@ -363,8 +367,8 @@ func main() {
 	j, err := json.Marshal(jData)
 	fmt.Println(string(j))
 
-	_, err = writer.Write(j)
-	writer.Flush()
+	_, err = output.Write(j)
+	output.Flush()
 
 	// fmt.Println(readStats.totals)
 	// fmt.Println(writeStats.totals)
@@ -378,4 +382,56 @@ func main() {
 	fmt.Print(writeStats.totals["DRV-Q"])
 	fmt.Print(", ")
 	fmt.Print(writeStats.totals["C-DRV"])
+}
+
+//#################
+//# main program flow starts here
+func main() {
+	// program arguments declaration
+	var args struct {
+		Action       string `arg:"positional,required,help:parse or report"`
+		Input        string `arg:"positional,required,help:specify the input blktrace file"`
+		Output       string `arg:"-o"`
+		OutFormat    string `arg:"--oformat,-f"`
+		ReportConfig string `arg:"-r"`
+	}
+
+	// default argument values
+	args.OutFormat = "default"
+
+	// parse arguments
+	arg.MustParse(&args)
+	fmt.Printf("%+v\n", args)
+
+	// TODO: try to open in/output files
+	fileInput, err := os.Open(args.Input)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer fileInput.Close()
+	reader := bufio.NewReader(fileInput)
+
+	var writer *bufio.Writer
+	if args.Output != "" {
+		fileOutput, err := os.OpenFile(os.Args[2], os.O_RDWR|os.O_CREATE, 0644)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		defer fileOutput.Close()
+		writer = bufio.NewWriter(fileOutput)
+	}
+
+	switch args.Action {
+	case "parse":
+		parse(reader, writer)
+		break
+	case "report":
+		// TODO: Check report config and than go on
+		report(reader, writer)
+		break
+	default:
+		println("main(): seems like something went wrong!!!")
+	}
 }
